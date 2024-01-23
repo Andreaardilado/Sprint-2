38902ddc900c47af392e25f1c84077779ae878e7
Andreaardilado <andreaardila@gmail.com>
2024-01-23 15:23:44 -0500

    creacion_java_script

---
+++ script/home.js
@@ -0,0 +1,29 @@
+const anillos = [
+ { 
+    id: 1, 
+    nombre: "Aurora Ring", 
+    codigo: "A0123",
+    precioUnitario: 125.28,
+    tipoAccesorio: "anillo",
+    imagenes: "../images/Aurora Ring.png",
+    descripcion: "Anillo diamantado olas",
+    cantidadColor:{
+       "Rose Gold":31,
+       "Silver": 38,
+    },
+    cantidadTalla:{
+        "48": 3,
+        "50": 7,
+        "52": 0,
+        "54": 6,
+        "56": 2,
+        "58": 9,
+        "60": 10,
+        "62": 4,
+        "64": 15,
+        "66": 5,
+        "68": 2,
+        "70": 6,
+    },
+},
+];
