bd53f0ccbbaaaabee2e59e1246a671cc350b16fa
Doraly Toledo <doratoland@gmail.com>
2024-01-23 19:34:40 -0500

    lista brazalete y collares

--- script/home.js
+++ script/home.js
@@ -27,3 +27,467 @@ const anillos = [
     },
 },
 ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const productos= [
+  {
+    id:  
+    nombre: "Bracelet", 
+    codigo: "B0123",
+    precioUnitario: 125.28,
+    tipoAccesorio: "Bracelet",
+    imagenes: "../images/",
+    descripcion: "Bracelet",
+    cantidadColor:{
+       "Rose Gold":0,
+       "Silver": 0,
+       "Gold": 0,
+    },
+    cantidadTalla:{
+        "48": 0,
+        "50": 0,
+        "52": 0,
+        "54": 0,
+        "56": 0,
+        "58": 0,
+        "60": 0,
+        "62": 0,
+        "64": 0,
+        "66": 0,
+        "68": 0,
+        "70": 0,
+},
+},
+];
+    
+const productos= [
+  {
+    id:  
+    nombre: "Luxury Gems Necklace", 
+    codigo: "N0123",
+    precioUnitario: 168.76,
+    tipoAccesorio: "Necklace",
+    imagenes: "../images/Luxury Gems Necklace.png",
+    descripcion: "Collar gema rosada",
+    cantidadColor:{
+      "Rose Gold":0,
+      "Silver": 0,
+      "Gold": 0,
+    },
+    cantidadTalla:{
+        "48": 0,
+        "50": 0,
+        "52": 0,
+        "54": 0,
+        "56": 0,
+        "58": 0,
+        "60": 0,
+        "62": 0,
+        "64": 0,
+        "66": 0,
+        "68": 0,
+        "70": 0,
+},
+},
+
+  {
+        id:  
+        nombre: "Reflections Necklace", 
+        codigo: "N0124",
+        precioUnitario:620.73,
+        tipoAccesorio: "Necklace",
+        imagenes: "../images/Reflections Necklace.png",
+        descripcion: "Collar serpiente rojo-negro-dorado",
+        cantidadColor:{
+          "Rose Gold":0,
+          "Silver": 0,
+          "Gold": 0,
+        },
+        cantidadTalla:{
+            "48": 0,
+            "50": 0,
+            "52": 0,
+            "54": 0,
+            "56": 0,
+            "58": 0,
+            "60": 0,
+            "62": 0,
+            "64": 0,
+            "66": 0,
+            "68": 0,
+            "70": 0,
+    },
+  },
+
+  {
+    id:  
+    nombre: "Radiance Necklace", 
+    codigo: "N0125",
+    precioUnitario:168.76,
+    tipoAccesorio: "Necklace",
+    imagenes:  "../images/Image 1(1).jpg" ,
+    descripcion: "collar doble dorado home",
+    cantidadColor:{
+      "Rose Gold":0,
+      "Silver": 0,
+      "Gold": 0,
+    },
+    cantidadTalla:{
+        "48": 0,
+        "50": 0,
+        "52": 0,
+        "54": 0,
+        "56": 0,
+        "58": 0,
+        "60": 0,
+        "62": 0,
+        "64": 0,
+        "66": 0,
+        "68": 0,
+        "70": 0,
+},
+},
+
+{
+  id:  
+  nombre: "Glamour Necklace", 
+  codigo: "N0126",
+  precioUnitario:620.73
+  tipoAccesorio: "Necklace",
+  imagenes:  "../images/Image 1 (3).jpg",
+  descripcion: "collar 4 circulos home",
+  cantidadColor:{
+    "Rose Gold":0,
+    "Silver": 0,
+    "Gold": 0,
+  },
+  cantidadTalla:{
+      "48": 0,
+      "50": 0,
+      "52": 0,
+      "54": 0,
+      "56": 0,
+      "58": 0,
+      "60": 0,
+      "62": 0,
+      "64": 0,
+      "66": 0,
+      "68": 0,
+      "70": 0,
+},
+},
+
+
+{
+  id:  
+  nombre: "Radiant Reflections", 
+  codigo: "N0127",
+  precioUnitario:
+  tipoAccesorio: "Necklace",
+  imagenes:  "../images/Image 4b.png",
+  descripcion: "collar esmeraldas",
+  cantidadColor:{
+       "Rose Gold":0,
+       "Silver": 0,
+       "Gold": 0,
+  },
+  cantidadTalla:{
+      "48": 0,
+      "50": 0,
+      "52": 0,
+      "54": 0,
+      "56": 0,
+      "58": 0,
+      "60": 0,
+      "62": 0,
+      "64": 0,
+      "66": 0,
+      "68": 0,
+      "70": 0,
+},
+},
+
+{
+  id:  
+  nombre: "Majestic Mementos", 
+  codigo: "N0128",
+  precioUnitario:
+  tipoAccesorio: "Necklace",
+  imagenes: "../images/Image 4d.png",
+  descripcion: "collar perlas colores",
+  cantidadColor:{
+       "Rose Gold":0,
+       "Silver": 0,
+       "Gold": 0,
+  },
+  
+  cantidadTalla:{
+      "48": 0,
+      "50": 0,
+      "52": 0,
+      "54": 0,
+      "56": 0,
+      "58": 0,
+      "60": 0,
+      "62": 0,
+      "64": 0,
+      "66": 0,
+      "68": 0,
+      "70": 0,
+},
+},
+
+
+       
+
+
+
+];
